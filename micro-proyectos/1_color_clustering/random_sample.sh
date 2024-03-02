1f410fc43bf3d91a87eb1421dd8dee7eb67e6a4a
David Mendez Acuna <damenac@gmail.com>
2024-03-02 14:05:21 +0100

    Restructured project

---
+++ micro-proyectos/1_color_clustering/random_sample.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+count=0
+selection=0
+
+cd datos/archive
+pwd
+
+for d in *; do
+    echo $d
+    for file in "$d"/*; do
+        if [ -f "$file" ]; then
+            ((count++))
+            if [ $(( $RANDOM % 5 )) -eq 0 ]; then
+                echo "$file"
+                ((selection++))
+            fi
+        fi
+    done
+done
+
+echo "count: " $count
+echo "selection: " $selection
